7c9ab4980248c9e1ed4ae724e2db5114b8ac677c
passionybr2003 <passionybr2003@gmail.com>
2015-11-26 18:45:37 +0530

    fix issues in sitemapgenerator

--- common/header.php
+++ common/header.php
@@ -1,4 +1,6 @@
-<?php 
+<?php
+error_reporting = E_ALL & ~E_NOTICE & ~E_DEPRECATED & ~E_STRICT;
+error_reporting(0);
 require 'classes/common_funs.php';
 $cf = new Commonfuns();
 require_once 'meta_file.php';
@@ -6,7 +8,7 @@ $fileName = basename($_SERVER['SCRIPT_NAME'],'.php');
 $title = $meta_data[$fileName]['title'];
 $description = $meta_data[$fileName]['description'];
 $keywords = $meta_data[$fileName]['keywords'];
-error_reporting = E_ALL & ~E_NOTICE & ~E_DEPRECATED & ~E_STRICT;
+
 
 
 ?>
